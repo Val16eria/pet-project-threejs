d565dbd537bfb3a904e94e9633ed54353bcfa1ad
Val16eria <KStorer61@icloud.com>
2024-11-02 12:55:27 +0300

    added a control panel

--- src/shared/ui/torusKnot/TorusKnot.tsx
+++ src/shared/ui/torusKnot/TorusKnot.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { MeshWobbleMaterial } from "@react-three/drei";
 import { MeshProps, TorusKnotGeometryProps } from "@react-three/fiber";
+import { Mesh } from "three";
+import { useControls } from "leva";
 
 interface ITorusKnot {
 	position: MeshProps["position"];
@@ -11,12 +13,23 @@ interface ITorusKnot {
 export const TorusKnot: FC<ITorusKnot> = ({
   position,
   size,
-  color
+  color: propColor
 }) => {
+  const ref = useRef<Mesh | null>(null);
+
+  const { color, radius } = useControls({
+    color: propColor,
+    radius: {
+      value: 5,
+	    min: 1,
+	    max: 10,
+	    step: 0.5
+    },
+  });
+
   return (
-    <mesh position={position}>
-      <torusKnotGeometry args={size} />
-      {/*<meshStandardMaterial color={color} />*/}
+    <mesh position={position} ref={ref}>
+      <torusKnotGeometry args={[radius, ...size]} />
 	    <MeshWobbleMaterial
 		    color={color}
 		    factor={5}
